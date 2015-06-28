83ba43efcdfdae15e3cc814722df319a59478cb8
Dmitry Ustalov <dmitry@eveel.ru>
2015-06-29 00:28:26 +0500

    Optimize InverseCountAllocator

--- src/main/java/mtsar/processors/task/InverseCountAllocator.java
+++ src/main/java/mtsar/processors/task/InverseCountAllocator.java
@@ -10,13 +10,13 @@ import mtsar.processors.TaskAllocator;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
-import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class InverseCountAllocator implements TaskAllocator {
-    public static final Comparator<Task> RANDOM_COMPARATOR = Comparator.comparing(t -> Math.random());
-
     protected final Provider<Process> process;
     protected final TaskDAO taskDAO;
     protected final AnswerDAO answerDAO;
@@ -30,14 +30,22 @@ public class InverseCountAllocator implements TaskAllocator {
 
     @Override
     public Optional<TaskAllocation> allocate(Worker worker) {
-        final Comparator<Task> comparator = getComparator(answerDAO).thenComparing(RANDOM_COMPARATOR);
-        final List<Task> tasks = taskDAO.listForProcess(process.get().getId());
-        final Optional<Task> task = tasks.stream().sorted(comparator).findFirst();
-        if (!task.isPresent()) return Optional.empty();
-        return Optional.of(new TaskAllocation(worker, task.get()));
-    }
+        List<Task> tasks = taskDAO.listForProcess(process.get().getId());
+        if (tasks.isEmpty()) return Optional.empty();
 
-    private Comparator<Task> getComparator(AnswerDAO answerDAO) {
-        return Comparator.comparing(task -> answerDAO.listForTask(task.getId(), process.get().getId()).size());
+        final Map<Task, Integer> counts = tasks.parallelStream().collect(
+                Collectors.toMap(Function.identity(), t -> answerDAO.listForTask(t.getId(), process.get().getId()).size())
+        );
+
+        tasks.sort((t1, t2) -> {
+            final int countsComparison = counts.get(t1).compareTo(counts.get(t2));
+            if (countsComparison == 0) {
+                return Math.random() > 0.5 ? 1 : -1;
+            } else {
+                return countsComparison;
+            }
+        });
+
+        return Optional.of(new TaskAllocation(worker, tasks.get(0)));
     }
 }
