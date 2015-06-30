0f6046c4441ffae0d102cd9e650947bf78a10156
Dmitry Ustalov <dmitry@eveel.ru>
2015-06-30 18:18:29 +0500

    Make InverseCountAllocator more flexible

--- src/main/java/mtsar/processors/task/InverseCountAllocator.java
+++ src/main/java/mtsar/processors/task/InverseCountAllocator.java
@@ -7,16 +7,28 @@ import mtsar.api.Worker;
 import mtsar.api.sql.AnswerDAO;
 import mtsar.api.sql.TaskDAO;
 import mtsar.processors.TaskAllocator;
+import org.apache.commons.collections.comparators.ComparatorChain;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class InverseCountAllocator implements TaskAllocator {
+    class AnswerCountComparator implements Comparator<Task> {
+        private final Map<Task, Integer> counts;
+
+        public AnswerCountComparator(Map<Task, Integer> counts) {
+            this.counts = counts;
+        }
+
+        @Override
+        public int compare(Task t1, Task t2) {
+            return counts.get(t1).compareTo(counts.get(t2));
+        }
+    }
+
     protected final Provider<Process> process;
     protected final TaskDAO taskDAO;
     protected final AnswerDAO answerDAO;
@@ -31,14 +43,30 @@ public class InverseCountAllocator implements TaskAllocator {
     @Override
     public Optional<TaskAllocation> allocate(Worker worker) {
         List<Task> tasks = taskDAO.listForProcess(process.get().getId());
+        final Map<Task, Integer> counts = getCounts(tasks, worker);
+
+        final Iterator<Task> iterator = tasks.iterator();
+        while (iterator.hasNext()) {
+            final Task task = iterator.next();
+            if (!counts.containsKey(task)) iterator.remove();
+        }
+
         if (tasks.isEmpty()) return Optional.empty();
-
-        final Map<Task, Integer> counts = tasks.parallelStream().collect(
-                Collectors.toMap(Function.identity(), t -> answerDAO.listForTask(t.getId(), process.get().getId()).size())
-        );
-
-        tasks.sort((t1, t2) -> counts.get(t1).compareTo(counts.get(t2)));
+        tasks.sort(getComparator(counts, worker));
 
         return Optional.of(new TaskAllocation(worker, tasks.get(0)));
+    }
+
+    protected Map<Task, Integer> getCounts(List<Task> tasks, Worker worker) {
+        return tasks.parallelStream().collect(
+                Collectors.toMap(Function.identity(), t -> answerDAO.listForTask(t.getId(), process.get().getId()).size())
+        );
+    }
+
+    private Comparator getComparator(Map<Task, Integer> counts, Worker worker) {
+        final ComparatorChain comparator = new ComparatorChain();
+        comparator.addComparator(new AnswerCountComparator(counts));
+        comparator.addComparator(Comparator.comparing(o -> o.hashCode() % worker.getId()));
+        return comparator;
     }
 }
