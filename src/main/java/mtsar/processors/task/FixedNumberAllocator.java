17a6e0a227bd7b9146539844663ada053fa5f357
Dmitry Ustalov <dmitry@eveel.ru>
2015-06-30 18:18:36 +0500

    Implement FixedNumberAllocator

--- src/main/java/mtsar/processors/task/FixedNumberAllocator.java
+++ src/main/java/mtsar/processors/task/FixedNumberAllocator.java
@@ -1,40 +1,49 @@
 package mtsar.processors.task;
 
 import mtsar.api.Process;
-import mtsar.api.TaskAllocation;
+import mtsar.api.Task;
 import mtsar.api.Worker;
 import mtsar.api.sql.AnswerDAO;
 import mtsar.api.sql.TaskDAO;
-import mtsar.processors.TaskAllocator;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
-import java.util.Optional;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-public class FixedNumberAllocator implements TaskAllocator {
-    protected final Provider<Process> process;
-    protected final TaskDAO taskDAO;
-    protected final AnswerDAO answerDAO;
-    protected int answersPerTask;
+public class FixedNumberAllocator extends InverseCountAllocator {
+    private Integer answersPerTask = null;
 
     @Inject
     public FixedNumberAllocator(Provider<Process> processProvider, TaskDAO taskDAO, AnswerDAO answerDAO) {
-        this.process = processProvider;
-        this.taskDAO = taskDAO;
-        this.answerDAO = answerDAO;
+        super(processProvider, taskDAO, answerDAO);
     }
 
     @Override
-    public Optional<TaskAllocation> allocate(Worker w) {
+    protected Map<Task, Integer> getCounts(List<Task> tasks, Worker worker) {
         ensureAnswersPerTask();
-        throw new UnsupportedOperationException("Not Implemented Yet");
+
+        final Map<Task, Boolean> answers = tasks.parallelStream().collect(
+                Collectors.toMap(Function.identity(), t -> answerDAO.listForTask(t.getId(), process.get().getId()).stream().anyMatch(a -> a.getWorkerId().equals(worker.getId())))
+        );
+
+        final Map<Task, Integer> counts = super.getCounts(tasks, worker);
+
+        final Iterator<Map.Entry<Task, Integer>> iterator = counts.entrySet().iterator();
+        while (iterator.hasNext()) {
+            final Map.Entry<Task, Integer> entry = iterator.next();
+            if (answers.get(entry.getKey()) || (entry.getValue() >= answersPerTask)) iterator.remove();
+        }
+
+        return counts;
     }
 
-    protected void ensureAnswersPerTask() {
+    private void ensureAnswersPerTask() {
         final Integer answersPerTask = Integer.valueOf(process.get().getOptions().get("answersPerTask"));
-        if (answersPerTask == null) {
-            throw new RuntimeException("answersPerTask option is not set");
-        }
+        if (answersPerTask == null) throw new RuntimeException("answersPerTask option is not set");
         this.answersPerTask = answersPerTask;
     }
 }
