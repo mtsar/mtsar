ebad7194a31b43abac3f94d5eb92ac2e384f2d27
Dmitry Ustalov <dmitry@eveel.ru>
2015-07-08 04:40:05 +0500

    Use mtsar.sh in Dockerfile [ci skip]

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 FROM maven:3-jdk-8
 
-RUN mkdir -p /usr/src/app
-WORKDIR /usr/src/app
+RUN mkdir -p /mtsar /var/log/mtsar
+WORKDIR /mtsar
 
-ADD pom.xml /usr/src/app/pom.xml
-RUN mvn verify clean -B -fn -Dmaven.test.skip=true
+ADD pom.xml /mtsar/pom.xml
+RUN mvn -T 4 verify clean -B -fn -Dmaven.test.skip=true
 
-ADD . /usr/src/app
-RUN mvn -B -Dmaven.test.skip=true verify; ln -sf target/mtsar-*.jar mtsar.jar
+ADD . /mtsar
+RUN mvn -T 4 -B -Dmaven.test.skip=true verify && ln -sf target/mtsar-*.jar mtsar.jar
 
 EXPOSE 8080 8081
 
-CMD ["java", "-jar", "mtsar.jar", "server", "environment.yml"]
+CMD ["/mtsar/mtsar.sh"]
---
+++ mtsar.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ -z "$POSTGRES_PORT_5432_TCP_ADDR" ]; then
+    POSTGRES_PORT_5432_TCP_ADDR=192.168.1.2
+fi
+
+if [ -z "$POSTGRES_DATABASE" ]; then
+    POSTGRES_DATABASE=mtsar
+fi
+
+if [ -z "$POSTGRES_USER" ]; then
+    POSTGRES_USER=mtsar
+fi
+
+if [ -z "$POSTGRES_PASSWORD" ]; then
+    POSTGRES_PASSWORD=mtsar
+fi
+
+if [ ! -z "$1" ]; then
+    export COMMAND=$@
+else
+    export COMMAND=server
+fi
+
+java \
+    -Ddw.database.url="jdbc:postgresql://$POSTGRES_PORT_5432_TCP_ADDR/$POSTGRES_DATABASE" \
+    -Ddw.database.user="$POSTGRES_USER" \
+    -Ddw.database.password="$POSTGRES_PASSWORD" \
+    -jar mtsar.jar $COMMAND mtsar.yml 2>>/var/log/mtsar/stderr.log >>/var/log/mtsar/stdout.log
