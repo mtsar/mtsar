f726abc39093c9f49b5d95b2bb809097f73b4226
Dmitry Ustalov <dmitry@eveel.ru>
2015-08-12 18:54:12 +0500

    Make the CSV writers shorter

--- src/main/java/mtsar/api/csv/AnswerCSVWriter.java
+++ src/main/java/mtsar/api/csv/AnswerCSVWriter.java
@@ -8,41 +8,24 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
 import java.util.List;
 
 public final class AnswerCSVWriter {
-    public static class AnswerIterator implements Iterator {
-        private final Iterator<Answer> answers;
-
-        public AnswerIterator(Iterable<Answer> answers) {
-            this.answers = answers.iterator();
-        }
-
-        @Override
-        public boolean hasNext() {
-            return answers.hasNext();
-        }
-
-        @Override
-        public Object[] next() {
-            final Answer answer = answers.next();
-            return new String[]{
-                    Integer.toString(answer.getId()),
-                    answer.getExternalId(),
-                    answer.getProcess(),
-                    Integer.toString(answer.getTaskId()),
-                    Integer.toString(answer.getWorkerId()),
-                    String.join("|", answer.getAnswers()),
-                    Long.toString(answer.getDateTime().toInstant().getEpochSecond())};
-        }
-    }
-
     public static final String[] HEADER = {"id", "external_id", "process", "worker_id", "task_id", "answers", "datetime"};
 
     public static void write(List<Answer> answers, OutputStream output) throws IOException {
         try (final Writer writer = new OutputStreamWriter(output, StandardCharsets.UTF_8)) {
-            CSVFormat.DEFAULT.withHeader(HEADER).print(writer).printRecords(new AnswerIterator(answers));
+            CSVFormat.DEFAULT.withHeader(HEADER).print(writer).printRecords(
+                    answers.stream().map(answer -> new String[]{
+                            Integer.toString(answer.getId()),
+                            answer.getExternalId(),
+                            answer.getProcess(),
+                            Integer.toString(answer.getTaskId()),
+                            Integer.toString(answer.getWorkerId()),
+                            String.join("|", answer.getAnswers()),
+                            Long.toString(answer.getDateTime().toInstant().getEpochSecond())
+                    }).iterator()
+            );
         }
     }
 }
--- src/main/java/mtsar/api/csv/WorkerCSVWriter.java
+++ src/main/java/mtsar/api/csv/WorkerCSVWriter.java
@@ -8,38 +8,21 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
 import java.util.List;
 
 public final class WorkerCSVWriter {
-    public static class WorkerIterator implements Iterator {
-        private final Iterator<Worker> workers;
-
-        public WorkerIterator(Iterable<Worker> workers) {
-            this.workers = workers.iterator();
-        }
-
-        @Override
-        public boolean hasNext() {
-            return workers.hasNext();
-        }
-
-        @Override
-        public Object[] next() {
-            final Worker worker = workers.next();
-            return new String[]{
-                    Integer.toString(worker.getId()),
-                    worker.getExternalId(),
-                    worker.getProcess(),
-                    Long.toString(worker.getDateTime().toInstant().getEpochSecond())};
-        }
-    }
-
     public static final String[] HEADER = {"id", "external_id", "process", "datetime"};
 
     public static void write(List<Worker> workers, OutputStream output) throws IOException {
         try (final Writer writer = new OutputStreamWriter(output, StandardCharsets.UTF_8)) {
-            CSVFormat.DEFAULT.withHeader(HEADER).print(writer).printRecords(new WorkerIterator(workers));
+            CSVFormat.DEFAULT.withHeader(HEADER).print(writer).printRecords(
+                    workers.stream().map(worker -> new String[]{
+                            Integer.toString(worker.getId()),
+                            worker.getExternalId(),
+                            worker.getProcess(),
+                            Long.toString(worker.getDateTime().toInstant().getEpochSecond())
+                    }).iterator()
+            );
         }
     }
 }
