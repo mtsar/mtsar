1cbfa25ee382f88798d0df6e1e8dfa8ffc73a398
Dmitry Ustalov <dmitry@eveel.ru>
2015-09-02 09:39:31 +0500

    Add a Karger, Oh & Shah algorithm implementation that does not work properly

--- src/main/java/mtsar/processors/answer/KOSAggregator.java
+++ src/main/java/mtsar/processors/answer/KOSAggregator.java
@@ -1,18 +1,31 @@
 package mtsar.processors.answer;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.HashBiMap;
+import com.google.common.collect.Table;
+import mtsar.api.Answer;
 import mtsar.api.AnswerAggregation;
 import mtsar.api.Process;
 import mtsar.api.Task;
 import mtsar.api.sql.AnswerDAO;
 import mtsar.api.sql.TaskDAO;
 import mtsar.processors.AnswerAggregator;
+import org.apache.commons.math3.distribution.NormalDistribution;
+import org.apache.commons.math3.distribution.RealDistribution;
 
 import javax.inject.Provider;
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static com.google.common.base.Preconditions.checkArgument;
 
 /**
  * Implementation of the answer aggregation algorithm proposed by Karger, Oh & Shah for binary tasks.
+ *
+ * This code is not verified, thus it provides random results and breaks unit tests.
+ * Do not use it now, please.
  *
  * @see <a href="http://pubsonline.informs.org/doi/abs/10.1287/opre.2013.1235">10.1287/opre.2013.1235</a>
  * @see MajorityVoting
@@ -30,6 +43,114 @@ public class KOSAggregator implements AnswerAggregator {
 
     @Override
     public Map<Task, AnswerAggregation> aggregate(Collection<Task> tasks) {
-        throw new UnsupportedOperationException("Not Implemented Yet");
+        if (tasks.isEmpty()) return Collections.emptyMap();
+
+        checkArgument(tasks.stream().allMatch(
+                task -> task.getAnswers().size() == 2 && task.getType() == TaskDAO.TASK_TYPE_SINGLE
+        ), "tasks should be of the type single and have only two possible answers");
+
+        final List<Answer> answers = answerDAO.listForProcess(process.get().getId());
+        if (answers.isEmpty()) return Collections.emptyMap();
+
+        final Map<Integer, Task> taskMap = taskDAO.listForProcess(process.get().getId()).stream().
+                filter(task -> task.getType() == TaskDAO.TASK_TYPE_SINGLE && task.getAnswers().size() == 2).
+                collect(Collectors.toMap(Task::getId, Function.identity()));
+
+        final Map<Integer, BiMap<String, Short>> answerIndex = taskMap.values().stream().collect(Collectors.toMap(Task::getId,
+                task -> {
+                    final BiMap<String, Short> map = HashBiMap.create(2);
+                    map.put(task.getAnswers().get(0), (short) -1);
+                    map.put(task.getAnswers().get(1), (short) +1);
+                    return map;
+                }
+        ));
+
+        /* rows are tasks IDs, columns are answer IDs, values are answers */
+        final Table<Integer, Integer, Short> graph = HashBasedTable.create();
+
+        for (final Answer answer : answers) {
+            if (answer.getType() != AnswerDAO.ANSWER_TYPE_ANSWER) continue;
+            graph.put(answer.getTaskId(), answer.getWorkerId(), answerIndex.get(answer.getTaskId()).get(answer.getAnswers().get(0)));
+        }
+
+        final Map<Integer, Double> estimations = converge(graph, 10);
+        return estimations.entrySet().stream().collect(Collectors.toMap(
+                estimation -> taskMap.get(estimation.getKey()),
+                estimation -> {
+                    final String answer = answerIndex.get(estimation.getKey()).inverse().get(estimation.getValue() < 0 ? (short) -1 : (short) +1);
+                    final Answer wrapper = new Answer.Builder().addAnswers(answer).buildPartial();
+                    return new AnswerAggregation(taskMap.get(estimation.getKey()), wrapper);
+                }
+        ));
+    }
+
+    protected Map<Integer, Double> converge(Table<Integer, Integer, Short> graph, int kMax) {
+        final RealDistribution distribution = new NormalDistribution(1, 1);
+
+        Table<Integer, Integer, Double> ys = HashBasedTable.create(graph.rowKeySet().size(), graph.columnKeySet().size());
+
+        for (final Table.Cell<Integer, Integer, Short> cell : graph.cellSet()) {
+            ys.put(cell.getRowKey(), cell.getColumnKey(), distribution.sample());
+        }
+
+        for (int k = 1; k <= kMax; k++) {
+            final Table<Integer, Integer, Double> xs = tasksUpdate(graph, ys);
+            if (k < kMax) ys = workersUpdate(graph, xs);
+        }
+
+        final Map<Integer, Double> estimations = new HashMap<>();
+
+        for (final Integer taskId : graph.rowKeySet()) {
+            double sumProduct = 0.0;
+
+            final Map<Integer, Double> workers = ys.row(taskId);
+            for (final Map.Entry<Integer, Double> worker : workers.entrySet()) {
+                sumProduct += graph.get(taskId, worker.getKey()) * worker.getValue();
+            }
+
+            estimations.put(taskId, sumProduct);
+        }
+
+        return estimations;
+    }
+
+    protected Table<Integer, Integer, Double> tasksUpdate(Table<Integer, Integer, Short> graph, Table<Integer, Integer, Double> ys) {
+        final Table<Integer, Integer, Double> xs = HashBasedTable.create(graph.rowKeySet().size(), graph.columnKeySet().size());
+
+        for (final Table.Cell<Integer, Integer, Short> cell : graph.cellSet()) {
+            double sumProduct = 0.0;
+
+            final int taskId = cell.getRowKey(), workerId = cell.getColumnKey();
+            final Map<Integer, Short> workers = graph.row(taskId);
+
+            for (final Map.Entry<Integer, Short> worker : workers.entrySet()) {
+                if (worker.getKey() == workerId) continue;
+                sumProduct += worker.getValue() * ys.get(taskId, worker.getKey());
+            }
+
+            xs.put(taskId, workerId, sumProduct);
+        }
+
+        return xs;
+    }
+
+    protected Table<Integer, Integer, Double> workersUpdate(Table<Integer, Integer, Short> graph, Table<Integer, Integer, Double> xs) {
+        final Table<Integer, Integer, Double> ys = HashBasedTable.create(graph.rowKeySet().size(), graph.columnKeySet().size());
+
+        for (final Table.Cell<Integer, Integer, Short> cell : graph.cellSet()) {
+            double sumProduct = 0.0;
+
+            final int taskId = cell.getRowKey(), workerId = cell.getColumnKey();
+            final Map<Integer, Short> tasks = graph.column(workerId);
+
+            for (final Map.Entry<Integer, Short> task : tasks.entrySet()) {
+                if (task.getKey() == taskId) continue;
+                sumProduct += task.getValue() * xs.get(task.getKey(), workerId);
+            }
+
+            ys.put(taskId, workerId, sumProduct);
+        }
+
+        return ys;
     }
 }
--- src/test/java/mtsar/answer/KOSAggregatorTest.java
+++ src/test/java/mtsar/answer/KOSAggregatorTest.java
@@ -1,5 +1,7 @@
 package mtsar.answer;
 
+import com.google.common.collect.Lists;
+import mtsar.api.Answer;
 import mtsar.api.AnswerAggregation;
 import mtsar.api.Process;
 import mtsar.api.Task;
@@ -13,14 +15,13 @@ import java.util.Collections;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
 public class KOSAggregatorTest {
     private static final TaskDAO taskDAO = mock(TaskDAO.class);
     private static final AnswerDAO answerDAO = mock(AnswerDAO.class);
     private static final mtsar.api.Process process = mock(Process.class);
-    private static final Task task = mock(Task.class);
+    private static final Task task1 = mock(Task.class), task2 = mock(Task.class);
     private static final KOSAggregator aggregator = new KOSAggregator(() -> process, taskDAO, answerDAO);
 
     @Before
@@ -28,15 +29,45 @@ public class KOSAggregatorTest {
         reset(taskDAO);
         reset(answerDAO);
         when(process.getId()).thenReturn("1");
-        when(task.getId()).thenReturn(1);
+        when(task1.getId()).thenReturn(1);
+        when(task1.getType()).thenReturn(TaskDAO.TASK_TYPE_SINGLE);
+        when(task1.getAnswers()).thenReturn(Lists.newArrayList("1", "2"));
+        when(task2.getId()).thenReturn(2);
+        when(task2.getType()).thenReturn(TaskDAO.TASK_TYPE_SINGLE);
+        when(task2.getAnswers()).thenReturn(Lists.newArrayList("a", "b"));
+    }
+
+    @Test
+    public void testTwoTasks() {
+        when(taskDAO.listForProcess(anyString())).thenReturn(Lists.newArrayList(task1, task2));
+        when(answerDAO.listForProcess(anyString())).thenReturn(Lists.newArrayList(
+                new Answer.Builder().setWorkerId(1).setTaskId(1).addAnswers("1").buildPartial(),
+                new Answer.Builder().setWorkerId(2).setTaskId(1).addAnswers("1").buildPartial(),
+                new Answer.Builder().setWorkerId(3).setTaskId(1).addAnswers("2").buildPartial(),
+                new Answer.Builder().setWorkerId(1).setTaskId(2).addAnswers("a").buildPartial(),
+                new Answer.Builder().setWorkerId(2).setTaskId(2).addAnswers("b").buildPartial(),
+                new Answer.Builder().setWorkerId(3).setTaskId(2).addAnswers("b").buildPartial()
+        ));
+        {
+            final Optional<AnswerAggregation> winner = aggregator.aggregate(task1);
+            assertThat(winner.isPresent());
+            final Answer answer = winner.get().getAnswer();
+            assertThat(answer.getAnswers()).hasSize(1);
+            assertThat(answer.getAnswer().get()).isEqualTo("1");
+        }
+        {
+            final Optional<AnswerAggregation> winner = aggregator.aggregate(task2);
+            assertThat(winner.isPresent());
+            final Answer answer = winner.get().getAnswer();
+            assertThat(answer.getAnswers()).hasSize(1);
+            assertThat(answer.getAnswer().get()).isEqualTo("b");
+        }
     }
 
     @Test
     public void testEmptyCase() {
-        when(answerDAO.listForTask(any(), anyString())).thenReturn(Collections.emptyList());
-        assertThatThrownBy(() -> {
-            final Optional<AnswerAggregation> winner = aggregator.aggregate(task);
-            assertThat(winner.isPresent()).isFalse();
-        }).isInstanceOf(UnsupportedOperationException.class).hasMessageContaining("Not Implemented Yet");
+        when(answerDAO.listForProcess(anyString())).thenReturn(Collections.emptyList());
+        final Optional<AnswerAggregation> winner = aggregator.aggregate(task1);
+        assertThat(winner.isPresent()).isFalse();
     }
 }
