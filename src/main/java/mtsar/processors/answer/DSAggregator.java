d5d95cd4187ef7909b5768708388967994f395df
Dmitry Ustalov <dmitry@eveel.ru>
2015-09-03 04:45:42 +0500

    Add a brilliant Dawid-Skene algorithm implementation by @ipeirotis

---
+++ src/main/java/mtsar/processors/answer/DSAggregator.java
@@ -0,0 +1,85 @@
+package mtsar.processors.answer;
+
+import com.ipeirotis.gal.algorithms.DawidSkene;
+import com.ipeirotis.gal.core.AssignedLabel;
+import com.ipeirotis.gal.core.Category;
+import com.ipeirotis.gal.core.Datum;
+import mtsar.api.Answer;
+import mtsar.api.AnswerAggregation;
+import mtsar.api.Process;
+import mtsar.api.Task;
+import mtsar.api.sql.AnswerDAO;
+import mtsar.api.sql.TaskDAO;
+import mtsar.processors.AnswerAggregator;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.stream.Collectors;
+
+/**
+ * A classical Dawid-Skene inference algorithm has been proposed far back in 1979.
+ * This aggregator is driven by the well-known implementation by Sheng, Provost & Ipeirotis.
+ *
+ * @see <a href="http://dl.acm.org/citation.cfm?id=1401965">10.1145/1401890.1401965</a>
+ * @see <a href="http://www.jstor.org/stable/2346806">10.2307/2346806</a>
+ */
+public class DSAggregator implements AnswerAggregator {
+    public static <T> Comparator<T> comparingDouble(ToDoubleFunction<? super T> keyExtractor) {
+        return Comparator.comparingDouble(keyExtractor).reversed();
+    }
+
+    private final Provider<Process> process;
+    private final TaskDAO taskDAO;
+    private final AnswerDAO answerDAO;
+
+    @Inject
+    public DSAggregator(Provider<Process> process, TaskDAO taskDAO, AnswerDAO answerDAO) {
+        this.process = process;
+        this.taskDAO = taskDAO;
+        this.answerDAO = answerDAO;
+    }
+
+    @Override
+    public Map<Task, AnswerAggregation> aggregate(Collection<Task> tasks) {
+        if (tasks.isEmpty()) return Collections.emptyMap();
+
+        final Map<Integer, Task> taskMap = taskDAO.listForProcess(process.get().getId()).stream().
+                collect(Collectors.toMap(Task::getId, Function.identity()));
+
+        final Set<Category> categories = taskMap.values().stream().
+                flatMap(task -> task.getAnswers().stream().map(answer -> new Category(answer))).
+                collect(Collectors.toSet());
+
+        final DawidSkene ds = new DawidSkene(categories);
+
+        final List<Answer> answers = answerDAO.listForProcess(process.get().getId());
+
+        for (final Answer answer : answers) {
+            if (!answer.getType().equalsIgnoreCase(AnswerDAO.ANSWER_TYPE_ANSWER)) continue;
+            for (final String label : answer.getAnswers()) {
+                ds.addAssignedLabel(new AssignedLabel(
+                        Integer.toString(answer.getWorkerId()),
+                        Integer.toString(answer.getTaskId()),
+                        label
+                ));
+            }
+        }
+
+        ds.estimate(tasks.size() <= 50 ? 50 : tasks.size(), 0.0001);
+
+        final Map<Task, AnswerAggregation> results = ds.getObjects().values().stream().collect(Collectors.toMap(
+                datum -> taskMap.get(Integer.valueOf(datum.getName())),
+                datum -> {
+                    final Task task = taskMap.get(Integer.valueOf(datum.getName()));
+                    final Map<String, Double> probabilities = datum.getProbabilityVector(Datum.ClassificationMethod.DS_Soft);
+                    final Map.Entry<String, Double> winner = probabilities.entrySet().stream().sorted(comparingDouble(Map.Entry::getValue)).findFirst().get();
+                    return new AnswerAggregation.Builder().setTask(task).addAnswers(winner.getKey()).build();
+                }
+        ));
+
+        return results;
+    }
+}
---
+++ src/test/java/mtsar/answer/DSAggregatorTest.java
@@ -0,0 +1,66 @@
+package mtsar.answer;
+
+import com.google.common.collect.Lists;
+import mtsar.api.Answer;
+import mtsar.api.AnswerAggregation;
+import mtsar.api.Task;
+import mtsar.api.sql.AnswerDAO;
+import mtsar.api.sql.TaskDAO;
+import mtsar.processors.AnswerAggregator;
+import mtsar.processors.answer.DSAggregator;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static mtsar.TestHelper.fixture;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
+
+public class DSAggregatorTest {
+    private static final TaskDAO taskDAO = mock(TaskDAO.class);
+    private static final AnswerDAO answerDAO = mock(AnswerDAO.class);
+    private static final mtsar.api.Process process = mock(mtsar.api.Process.class);
+    private static final Task task1 = fixture("task1.json", Task.class);
+    private static final Task task2 = fixture("task2.json", Task.class);
+    private static final AnswerAggregator aggregator = new DSAggregator(() -> process, taskDAO, answerDAO);
+
+    @Before
+    public void setup() {
+        reset(taskDAO);
+        reset(answerDAO);
+        when(process.getId()).thenReturn("1");
+    }
+
+    @Test
+    public void testTwoTasks() {
+        when(taskDAO.listForProcess(anyString())).thenReturn(Lists.newArrayList(task1, task2));
+        when(answerDAO.listForProcess(anyString())).thenReturn(Lists.newArrayList(
+                new Answer.Builder().setWorkerId(1).setTaskId(1).addAnswers("1").buildPartial(),
+                new Answer.Builder().setWorkerId(2).setTaskId(1).addAnswers("1").buildPartial(),
+                new Answer.Builder().setWorkerId(1).setTaskId(2).addAnswers("1").buildPartial(),
+                new Answer.Builder().setWorkerId(2).setTaskId(2).addAnswers("2").buildPartial()
+        ));
+        {
+            final Optional<AnswerAggregation> winner = aggregator.aggregate(task1);
+            assertThat(winner.isPresent());
+            assertThat(winner.get().getAnswers()).hasSize(1);
+            assertThat(winner.get().getAnswers().get(0)).isEqualTo("1");
+        }
+        {
+            final Optional<AnswerAggregation> winner = aggregator.aggregate(task2);
+            assertThat(winner.isPresent());
+            assertThat(winner.get().getAnswers()).hasSize(1);
+            assertThat(winner.get().getAnswers().get(0)).isEqualTo("1");
+        }
+    }
+
+    @Test
+    public void testEmptyCase() {
+        when(answerDAO.listForProcess(anyString())).thenReturn(Collections.emptyList());
+        final Optional<AnswerAggregation> winner = aggregator.aggregate(task1);
+        assertThat(winner.isPresent()).isFalse();
+    }
+}
