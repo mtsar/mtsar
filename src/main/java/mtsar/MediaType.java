1eeb9c193c01548c900b729ea4f511b972f5e454
Dmitry Ustalov <dmitry@eveel.ru>
2015-09-16 22:17:40 +0500

    Force UTF-8 in TEXT_CSV [ci skip]

--- src/main/java/mtsar/MediaType.java
+++ src/main/java/mtsar/MediaType.java
@@ -18,5 +18,5 @@ package mtsar;
 
 public final class MediaType {
     public final static String APPLICATION_JSON = javax.ws.rs.core.MediaType.APPLICATION_JSON + "; charset=UTF-8";
-    public final static String TEXT_CSV = "text/csv";
+    public final static String TEXT_CSV = "text/csv; charset=UTF-8";
 }
