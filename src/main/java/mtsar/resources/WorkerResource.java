ca32d431df3626c61792bec7e0d840829c3d1a6b
Dmitry Ustalov <dmitry@eveel.ru>
2016-02-24 00:36:15 +0500

    Yep, workers/rankings should produce JSON [ci skip]

--- src/main/java/mtsar/resources/WorkerResource.java
+++ src/main/java/mtsar/resources/WorkerResource.java
@@ -75,7 +75,6 @@ public class WorkerResource {
 
     @GET
     @Path("rankings")
-    @Produces(mtsar.util.MediaType.TEXT_CSV)
     public Map<Integer, WorkerRanking> getWorkerRankings() {
         final List<Worker> workers = workerDAO.listForStage(stage.getId());
         final Map<Integer, WorkerRanking> rankings = stage.getWorkerRanker().rank(workers);
